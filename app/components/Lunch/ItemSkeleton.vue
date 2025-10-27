385fe5c7c556352f43746d98ed3646eb6b580c92
Kjek <kjek@me.com>
2025-10-27 23:32:22 +0100

    style: add skeleton to indicate loading

---
+++ app/components/Lunch/ItemSkeleton.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="border-b p-6 break-words whitespace-pre-line last:border-none sm:flex">
+    <div class="flex w-1/3 justify-center">
+      <USkeleton
+        as="h2"
+        class="h-9 w-42"
+      />
+    </div>
+    <div class="sm:w-2/3 sm:grow">
+      <UPageList
+        class="gap-4"
+        as="ul"
+      >
+        <li>
+          <div class="flex pb-2">
+            <USkeleton
+              class="h-8 w-32 text-2xl font-bold"
+              as="h3"
+            />
+          </div>
+          <div class="flex flex-col gap-2">
+            <USkeleton
+              class="h-7 grow pb-2 text-lg"
+              as="p"
+            />
+            <USkeleton
+              class="h-7 w-1/3 text-lg"
+              as="p"
+            />
+          </div>
+        </li>
+      </UPageList>
+      <UPageList
+        class="flex flex-col gap-4 pt-6"
+        as="ul"
+      >
+        <li>
+          <div class="flex flex-col gap-2">
+            <USkeleton
+              class="h-6 w-60"
+              as="h4"
+            />
+            <USkeleton
+              class="h-7 grow"
+              as="h5"
+            />
+          </div>
+        </li>
+        <li>
+          <div class="flex flex-col gap-2">
+            <USkeleton
+              class="h-6 w-60"
+              as="h4"
+            />
+            <USkeleton
+              class="h-7 grow"
+              as="h5"
+            />
+          </div>
+        </li>
+      </UPageList>
+    </div>
+  </div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,27 +2,39 @@
   <UContainer>
     <FilterContainer />
     <UPageList
-      v-if="restaurants.length > 0"
       as="ul"
       class="gap-4 pb-32 md:pb-8"
     >
-      <UPageCard
-        v-for="restaurant in restaurants"
-        :key="restaurant.name"
-        as="li"
-        class="bg-neutral-50 dark:bg-neutral-950"
-      >
-        <LunchItem
-          :restaurant="restaurant"
-          data-test="lunch-item"
-        />
-      </UPageCard>
+      <template v-if="restaurants.length > 0">
+        <UPageCard
+          v-for="restaurant in restaurants"
+          :key="restaurant.name"
+          as="li"
+          class="bg-neutral-50 dark:bg-neutral-950"
+        >
+          <LunchItem
+            :restaurant="restaurant"
+            data-test="lunch-item"
+          />
+        </UPageCard>
+      </template>
+      <template v-else>
+        <UPageCard
+          v-for="item in 2"
+          v-show="restaurants.length === 0"
+          :key="item"
+          class="bg-neutral-50 dark:bg-neutral-950"
+        >
+          <ItemSkeleton />
+        </UPageCard>
+      </template>
     </UPageList>
   </UContainer>
 </template>
 
 <script lang="ts" setup>
 import { storeToRefs } from 'pinia';
+import ItemSkeleton from '~/components/Lunch/ItemSkeleton.vue';
 import { useRestaurantsStore } from '~/stores/use-restaurants';
 
 const restaurantsStore = useRestaurantsStore();
