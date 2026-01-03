7e0f0bcc3f4c160caad218728b58e30d5e2799b9
Kjek <kjek@me.com>
2026-01-03 03:05:10 +0100

    chore: fix week not being updated properly

--- app/plugins/date.ts
+++ app/plugins/date.ts
@@ -2,8 +2,14 @@ import { sweDays } from '~/types/swedish-days';
 
 export default defineNuxtPlugin(() => {
   Date.prototype.getWeek = function () {
-    const weekOne = new Date(this.getFullYear(), 0, 4);
-    return Math.ceil(((this.valueOf() - weekOne.valueOf()) / 86400000 + weekOne.getDay() - 1) / 7);
+    const date = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));
+    const dayNum = date.getUTCDay() || 7;
+
+    // The ISO week-numbering year is the year that contains the Thursday of that week
+    date.setUTCDate(date.getUTCDate() + 4 - dayNum);
+
+    const yearStart = new Date(Date.UTC(date.getUTCFullYear(), 0, 1));
+    return Math.ceil(((date.valueOf() - yearStart.valueOf()) / 86400000 + 1) / 7);
   };
 
   Date.prototype.today = function () {
