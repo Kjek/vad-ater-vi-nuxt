9a5bb6244a46f7c4ec51380d709bc1ae8c703528
Kjek <kjek@me.com>
2026-01-29 22:23:30 +0100

    chore: add more logs & remove headers

--- server/scrapers/generic.ts
+++ server/scrapers/generic.ts
@@ -13,17 +13,9 @@ import { toSentenceCase } from '../utils/string-utils';
 const genericWebScraper: Scraper = async (lunchUrl, lunchRegex, weeklyRegex, debug) => {
   console.time(`Generic scraper for ${lunchUrl}`);
   const HEADERS: HeadersInit = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0',
-    Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-    'Accept-Language': 'en-US,en;q=0.5',
+    'User-Agent': 'Mozilla/5.0',
+    Accept: 'text/html',
     'Accept-Encoding': 'gzip, deflate',
-    Connection: 'keep-alive',
-    'Upgrade-Insecure-Requests': '1',
-    'Sec-Fetch-Dest': 'document',
-    'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-Site': 'none',
-    'Sec-Fetch-User': '?1',
-    'Cache-Control': 'max-age=0',
   };
 
   const response = await fetch(lunchUrl, {
@@ -32,7 +24,8 @@ const genericWebScraper: Scraper = async (lunchUrl, lunchRegex, weeklyRegex, deb
 
   if (!response.ok) {
     console.timeEnd(`Generic scraper for ${lunchUrl}`);
-    throw new Error(`${response.status}: ${response.statusText}`);
+    const text = await response.text();
+    throw new Error(`HTTP ${response.status}\n${text.slice(0, 500)}`);
   }
 
   const html = await response.text();
